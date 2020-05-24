1dae1b8d62f3509953794f1b232db86f7aa23e31
evgeny-ilin <evgeny.ilin@gmail.com>
2020-05-24 12:56:12 +0400

    HW0 feat: add UserMealsUtil.filteredByCycles implementation with O(n log(n)) time complexity

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.time.temporal.ChronoField;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -28,8 +29,55 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+        // return filtered list with excess. Implement by cycles
+        meals.sort(Comparator.comparing(UserMeal::getDateTime)); //O(n log(n))
+
+        List<UserMealWithExcess> filteredMeals = new ArrayList<>();
+        List<LocalDate> excessDays = getExcessDays(meals,caloriesPerDay);
+
+        //O(n+m), в худшем случае мы каждый день превысили норму m=n -> O(n+m) = O(2n) = O(n)
+        for (UserMeal meal:meals) {//O(n)
+            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                LocalDate mealDay = LocalDate.from(meal.getDateTime());
+                boolean excess = excessDays.contains(mealDay); //O(m)
+                UserMealWithExcess mealWithExcess = new UserMealWithExcess(meal.getDateTime(),
+                        meal.getDescription(),
+                        meal.getCalories(),
+                        excess);
+                filteredMeals.add(mealWithExcess);//O(n), можно сделать O(1), если сразу создать список размером meals.size
+            }
+        }
+
+        return filteredMeals;
+    }
+
+    private static List<LocalDate> getExcessDays (List<UserMeal> meals, int caloriesPerDay) {
+
+        if (meals == null || meals.isEmpty()) return new ArrayList<LocalDate>();
+
+        List<LocalDate> excessDays = new ArrayList<>();
+        int sumCalories = 0;
+        LocalDate day = LocalDate.from(meals.get(0).getDateTime());
+
+
+        //O(n)
+        for (UserMeal meal:meals
+             ) {
+            LocalDate mealDay = LocalDate.from(meal.getDateTime());
+            if (mealDay.compareTo(day) == 0)
+                sumCalories += meal.getCalories();
+            else {
+                if (sumCalories > caloriesPerDay)
+                    excessDays.add(mealDay);
+                day = mealDay;
+                sumCalories = meal.getCalories();
+            }
+        }
+
+        if (sumCalories > caloriesPerDay)
+            excessDays.add(day);
+
+        return excessDays;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
