3a9d6f002e6c1c539ac0bdc42b7cb626b171f70e
evgeny-ilin <evgeny.ilin@gmail.com>
2020-06-05 09:14:58 +0400

    HW1: 1.2 feat list MealsTo on meals.jsp

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.service.MealServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,15 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
+    private MealService mealService = new MealServiceImpl();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-        response.sendRedirect("meals.jsp");
+        List<MealTo> mealToList = mealService.getMealsTo(LocalTime.of(0,0), LocalTime.of(23,59),2000);
+        request.setAttribute("mealToList", mealToList);
+        request.getRequestDispatcher("/meals.jsp").forward(request,response);
     }
 }
